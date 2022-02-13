cb9d6dc2c439194f0b5907185970e85fef67d22e
prxtik69 <pratikdeshpande007pvd@gmail.com>
2022-02-13 16:57:09 +0530

    changed staff password

--- Liberary_app.py
+++ Liberary_app.py
@@ -1,5 +1,5 @@
 class Liberary():
-    def __init__(self , list , name):
+    def __init__(self, list, name):
         self.BookList = list
         self.name = name
         self.lendDictionary = {}
@@ -13,19 +13,20 @@ class Liberary():
         self.BookList.append(book)
         print(f"Added {book} to the Book List")
 
-    def LendBook(self, book , name):
+    def LendBook(self, book, name):
         if book not in self.BookList:
-            print(f"We dont have this book in the Library , we have these Books Right Now!{self.BookList}")
+            print(
+                f"We dont have this book in the Library , we have these Books Right Now!{self.BookList}")
             exit()
         elif book not in self.lendDictionary.keys():
-                print(f"{name}We have updated our Database , Please take the book")
+            print(f"{name}We have updated our Database , Please take the book")
         else:
             print(f"The Book is already owner by {self.lendDictionary[book]}")
-    
+
     def returnBook(self, book):
         self.lendDictionary.pop(book)
 
-    def CheckBook(self , book):
+    def CheckBook(self, book):
         if book in self.lendDictionary.keys():
             print(f"This Book is Lended by {self.lendDictionary[book]}")
         else:
@@ -35,9 +36,10 @@ class Liberary():
         for xyz in self.lendDictionary:
             print(f"{xyz} Book is lended by {self.lendDictionary[xyz]}")
 
-PasswordForStaff = 'baspankapyar'
+
+PasswordForStaff = 'kachabadam'
 if __name__ == '__main__':
-    Pratik = Liberary(["Book1" , "Book2" , "Book3" , "Book4"] , "Pratik")
+    Pratik = Liberary(["Book1", "Book2", "Book3", "Book4"], "Pratik")
 
     while(True):
         print("Welcome to Liberary , Please Enter your choice")
@@ -51,26 +53,30 @@ if __name__ == '__main__':
         user_choice = int(input())
 
         # if user_choice not in ["1" , "2" , "3" , "4"]:
-            # print("Please Enter Valid Choice")
+        # print("Please Enter Valid Choice")
 
         if user_choice == 1:
             Pratik.DisplayBooks()
-        
+
         elif user_choice == 2:
-            AddBook_input = input("Enter The Name of Book , you want Add in Liberary : \n")
+            AddBook_input = input(
+                "Enter The Name of Book , you want Add in Liberary : \n")
             Pratik.AddBook(AddBook_input)
 
         elif user_choice == 3:
-            lendInputBook = input("Enter the Name of Book , you want to Lend : \n")
+            lendInputBook = input(
+                "Enter the Name of Book , you want to Lend : \n")
             lendInputUser = input("Enter Your Name : \n")
-            Pratik.LendBook(lendInputBook , lendInputUser)
+            Pratik.LendBook(lendInputBook, lendInputUser)
 
         elif user_choice == 4:
-            returnBookInput = input("Enter the name of Book , you want to retur : \n")
+            returnBookInput = input(
+                "Enter the name of Book , you want to retur : \n")
             Pratik.returnBook(returnBookInput)
 
         elif user_choice == 5:
-            TakeBookInput = input("Enter the name of Book , you want to check : \n")
+            TakeBookInput = input(
+                "Enter the name of Book , you want to check : \n")
             Pratik.CheckBook(TakeBookInput)
 
         elif user_choice == 6:
@@ -84,8 +90,6 @@ if __name__ == '__main__':
         else:
             print("Please enter Valid Option")
 
-
-
         print('Press c to Continue and q to Quit')
 
         user_choice2 = ""
@@ -96,4 +100,3 @@ if __name__ == '__main__':
 
             elif user_choice2 == "c" or user_choice2 == "C":
                 continue
-
